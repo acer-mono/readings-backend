e5434d2f259c1ae51ce314b29eace310c8415bfd
acer-mono <margaritastar92@gmail.com>
2021-05-01 16:40:24 +0700

    fix readings route

--- routes/ReadingListResource.py
+++ routes/ReadingListResource.py
@@ -1,3 +1,4 @@
+from flask import request
 from models.Reading import Reading
 from flask_restx import Resource
 from schemas.ReadingSchema import readings_schema
@@ -7,5 +8,9 @@ from flask_jwt_extended import jwt_required
 class ReadingListResource(Resource):
     @jwt_required()
     def get(self):
-        readings = Reading.query.all()
+        start_date = request.args.get('from')
+        end_date = request.args.get('to')
+        room_id = request.args.get('room')
+
+        readings = Reading.query.filter(Reading.date.between(start_date, end_date), Reading.room_id == room_id)
         return readings_schema.dump(readings)
