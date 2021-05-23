8f463da53e38f7f42c4bd789dad723f800713d99
acer-mono <margaritastar92@gmail.com>
2021-05-23 19:19:57 +0700

    show user list

--- routes/UserListResource.py
+++ routes/UserListResource.py
@@ -2,10 +2,13 @@ from models.User import User
 from flask_restx import Resource
 from schemas.UserSchema import users_schema
 import flask_praetorian
+from sqlalchemy import and_
 
 
 class UserListResource(Resource):
     @flask_praetorian.auth_required
     def get(self):
-        users = User.query.all()
+        user_id = flask_praetorian.current_user().id
+        conds = [User.id != user_id]
+        users = User.query.filter(and_(*conds))
         return users_schema.dump(users)
